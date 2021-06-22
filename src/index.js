a7a4de06face9a8293545ad484a305317594eaff
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-06-22 11:07:03 -0300

    Criaçao dos arquivos iniciais

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './css/reset.css';
+
+
+
+function App(){
+
+    return(
+        <div>Olá!</div>
+    )
+
+}
+
+ReactDOM.render(<App/>, document.querySelector(".root"));
